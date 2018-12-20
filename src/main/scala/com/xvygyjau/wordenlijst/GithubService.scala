5976e81ce38311520e8858bec0da1fc79b54b41a
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2018-12-21 00:16:58 +0100

    Add dumb error handling

--- src/main/scala/com/xvygyjau/wordenlijst/GithubService.scala
+++ src/main/scala/com/xvygyjau/wordenlijst/GithubService.scala
@@ -7,18 +7,28 @@ import org.http4s.HttpService
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
+import scala.util.{Failure, Success, Try}
+
 class GithubService[F[_]: Effect] extends Http4sDsl[F] {
 
   val service: HttpService[F] = {
     HttpService[F] {
       case GET -> Root / "token" / token =>
-        import Ids.hashids
-        val accessToken = github.AccessToken(token)
-        val apiKey = AccessToken.encode(accessToken)
-        Ok(
-          Json.obj("message" -> Json.fromString(
-                     s"Hello, unknown Github user, your api key is $apiKey"),
-                   "apiKey" -> Json.fromString(apiKey)))
+        Try {
+          import Ids.hashids
+          val accessToken = github.AccessToken(token)
+          AccessToken.encode(accessToken)
+        } match {
+          case Success(apiKey) =>
+            Ok(
+              Json.obj(
+                "message" -> Json.fromString(
+                  s"Hello, unknown Github user, your api key is $apiKey"),
+                "apiKey" -> Json.fromString(apiKey)))
+          case Failure(exception) =>
+            InternalServerError(Json.obj(
+              "message" -> Json.fromString(exception.getMessage)))
+        }
     }
   }
 }
